932b0295950ee73ca0822a3d33c99a21ba7b3e42
潘胜银 <panshengjin77@126.com>
2019-09-05 20:13:58 +0800

    dialog project

---
+++ dialog/dialog.cpp
@@ -0,0 +1,39 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QString>
+#include <QDebug>
+const static double PI= 3.1416;
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+
+void Dialog::on_countBtn_clicked()
+{
+    bool ok;
+    QString tempStr;
+    QString valueStr = ui->radiusLineEdit->text();
+    int valueInt = valueStr.toInt(&ok);
+    double area = valueInt * valueInt * PI;
+    ui->areaShowLabel->setText(tempStr.setNum(area));
+
+}
+
+void Dialog::on_radiusLineEdit_textChanged(const QString &arg1)
+{
+    bool ok;
+    QString tempStr;
+    QString valueStr = ui->radiusLineEdit->text();
+    int valueInt = valueStr.toInt(&ok);
+    double area = valueInt * valueInt * PI;
+    ui->areaShowLabel->setText(tempStr.setNum(area));
+}
